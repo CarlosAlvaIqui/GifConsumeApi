aaa70f896949827d1b8cf37ade4160a4f7d34cdc
CarlosAlvaIqui <carlos.alvarez.i@tecsup.edu.pe>
2021-06-29 21:14:44 -0500

    docs agregados

---
+++ docs/precache-manifest.7bc91a99f4760d0826c8d1aab52ec9fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2438ad5b6e3931504202bab1be71404",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2a36ac5f7e203dcd6dc",
+    "url": "/static/css/main.e0b36840.chunk.css"
+  },
+  {
+    "revision": "12aa159ad6a53351ca3b",
+    "url": "/static/js/2.e922ea24.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.e922ea24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a36ac5f7e203dcd6dc",
+    "url": "/static/js/main.fe55d063.chunk.js"
+  },
+  {
+    "revision": "fcdb2dd8f63dd0f52bfa",
+    "url": "/static/js/runtime-main.028153bf.js"
+  }
+]);
